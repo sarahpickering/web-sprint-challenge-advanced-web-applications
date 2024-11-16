fc34c7b17f711970289edc10dae1c58b7c12f525
Sarah Pickering <sarahpsellsaz@gmail.com>
2024-11-15 17:38:17 -0700

    Articles

--- frontend/components/Articles.js
+++ frontend/components/Articles.js
@@ -4,13 +4,24 @@ import PT from 'prop-types'
 
 export default function Articles(props) {
   // ✨ where are my props? Destructure them here
-
+  const {
+    articles,
+    getArticles,
+    deleteArticle,
+    currentArticleId,
+    setCurrentArticleId
+  } = props
   // ✨ implement conditional logic: if no token exists
   // we should render a Navigate to login screen (React Router v.6)
 
+  if (!localStorage.getItem('token')) {
+    return <Navigate to='/' />
+  }
+
   useEffect(() => {
     // ✨ grab the articles here, on first render only
-  })
+    getArticles()
+  }, [])
 
   return (
     // ✨ fix the JSX: replace `Function.prototype` with actual functions
